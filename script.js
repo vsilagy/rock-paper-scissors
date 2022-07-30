b7f3ed839d0596c17e39eef3c9109f7309146ad3
Vlad Silagy <vladsilagy@gmail.com>
2022-07-29 23:20:36 -0400

    Added functions for computer pick, player pick input and play round. the game is  working in console

---
+++ script.js
@@ -0,0 +1,33 @@
+const picks = ["rock", "paper", "scissors"];
+
+function game() {
+  playRound();
+}
+
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) {
+    console.log(`It's a tie`);
+  } else if (
+    (playerSelection === "paper" && computerSelection === "rock") ||
+    (playerSelection === "rock" && computerSelection === "scissors") ||
+    (playerSelection === "scissors" && computerSelection === "paper")
+  ) {
+    console.log(`You win!`);
+  } else {
+    console.log(`You Lose!`);
+  }
+}
+const computerSelection = computerPick();
+const playerSelection = playerPick();
+
+function computerPick() {
+  return picks[Math.floor(Math.random() * picks.length)];
+}
+
+function playerPick() {
+  let input = prompt("Type Rock, Paper or Scissors").toLowerCase();
+  console.log(input);
+  return input;
+}
+
+game();
