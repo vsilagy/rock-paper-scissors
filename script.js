e5149373dde366f42dde0bb48e8a2c416697f6bb
Vlad Silagy <vladsilagy@gmail.com>
2022-08-02 20:20:30 -0400

    added player pick section, view result section and footer

--- script.js
+++ script.js
@@ -1,31 +1,50 @@
-const picks = ["rock", "paper", "scissors"];
+const picks = document.querySelectorAll(".pick");
+const playerScoreDisplay = document.getElementById("score");
+const computerScoreDisplay = document.getElementById("computer");
+const main = document.getElementById("main");
+const header = document.getElementById("header");
+const result = document.getElementById("result");
+const playAgainBtn = document.getElementById("play-again");
+const playerChoice = document.getElementById("player-choice");
+const computerChoice = document.getElementById("computer-choice");
 
-function game() {
-  playRound();
-}
+let playerScore = 0;
+let houseScore = 0;
+let playerPick = undefined;
 
-function playRound(playerSelection, computerSelection) {
-  if (playerSelection === computerSelection) {
-    console.log(`It's a tie`);
-  } else if (
-    (playerSelection === "paper" && computerSelection === "rock") ||
-    (playerSelection === "rock" && computerSelection === "scissors") ||
-    (playerSelection === "scissors" && computerSelection === "paper")
-  ) {
-    console.log(`You win!`);
-  } else {
-    console.log(`You Lose!`);
-  }
-}
-const computerSelection = computerPick();
-const playerSelection = playerPick();
+picks.forEach((button) => {
+  button.addEventListener("click", () => {
+    playerPick = button.getAttribute("id");
+    console.log(playerPick);
+  });
+});
 
-function computerPick() {
-  return picks[Math.floor(Math.random() * picks.length)];
-}
+// function game() {
+//   playRound();
+// }
 
-function playerPick() {
-  return "rock";
-}
+// function playRound(playerSelection, computerSelection) {
+//   if (playerSelection === computerSelection) {
+//     console.log(`It's a tie`);
+//   } else if (
+//     (playerSelection === "paper" && computerSelection === "rock") ||
+//     (playerSelection === "rock" && computerSelection === "scissors") ||
+//     (playerSelection === "scissors" && computerSelection === "paper")
+//   ) {
+//     console.log(`You win!`);
+//   } else {
+//     console.log(`You Lose!`);
+//   }
+// }
+// const computerSelection = computerPick();
+// const playerSelection = playerPick();
 
-game();
+// function computerPick() {
+//   return picks[Math.floor(Math.random() * picks.length)];
+// }
+
+// function playerPick() {
+//   return "rock";
+// }
+
+// game();
